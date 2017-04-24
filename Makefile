12ff38b3db552f2041347ca55c8149ed6ccacf0a
Kendyll Kraus <kkraus1@nd.edu>
2017-04-24 19:38:21 -0400

    Makefile done, thor started

--- Makefile
+++ Makefile
@@ -6,6 +6,40 @@ TARGETS=	spidey
 
 all:		$(TARGETS)
 
+forking.o: forking.c spidey.h
+	@echo Compiling forking.o...
+	@$(CC) $(CFLAGS) -o $@ $<
+
+handler.o: handler.c spidey.h
+	@echo Compiling handler.o...
+	@$(CC) $(CFLAGS) -o $@ $<
+
+request.o: request.c spidey.h
+	@echo Compiling request.o...
+	@$(CC) $(CFLAGS) -o $@ $<
+
+single.o: single.c spidey.h
+	@echo Compiling single.o...
+	@$(CC) $(CFLAGS) -o $@ $<
+
+socket.o: socket.c spidey.h
+	@echo Compiling socket.o...
+	@$(CC) $(CFLAGS) -o $@ $<
+
+spidey.o: spidey.c spidey.h
+	@echo Compiling spidey.o...
+	@$(CC) $(CFLAGS) -o $@ $<
+
+utils.o: utils.c spidey.h
+	@echo Compiling utils.o...
+	@$(CC) $(CFLAGS) -o $@ $<
+
+spidey: forking.o handler.o request.o single.o socket.o spidey.o utils.o
+	@echo Linking spidey...
+	@$(LD) $(LDFLAGS) -o $@ $^
+	
+
+
 clean:
 	@echo Cleaning...
 	@rm -f $(TARGETS) *.o *.log *.input
--- thor.py
+++ thor.py
@@ -7,7 +7,7 @@ import sys
 import time
 
 # Globals
-
+ARGUMENTS = sys.argv[1:]
 PROCESSES = 1
 REQUESTS  = 1
 VERBOSE   = False
@@ -26,14 +26,39 @@ def usage(status=0):
     sys.exit(status)
 
 def do_request(pid):
-    pass
+    #r = requests.get(URL).text.rstrip()
+    start = time.time()
+    r = requests.get(URL)
+    end = time.time()
+    if VERBOSE:
+        print r.text.rstrip()
+    return (end - start)
 
 # Main execution
 
 if __name__ == '__main__':
     # Parse command line arguments
+    while len(ARGUMENTS) and ARGUMENTS[0].startswith('-') and len(ARGUMENTS[0]) > 1:
+        arg = ARGUMENTS.pop(0)
+        if arg == '-h':
+            usage(0)
+        elif arg == '-v':
+            VERBOSE = True
+        elif arg == '-p':
+            PROCESSES = ARGUMENTS.pop(0)
+        elif arg == '-r':
+            REQUESTS = ARGUMENTS.pop(0)
+        else:
+            usage(1)
+
+    URL = ARGUMENTS.pop(0)
+
 
     # Create pool of workers and perform requests
-    pass
+    pool = multiprocessing.Pool(PROCESSES)
+    pool.map(do_request, URL, REQUESTS)
+
+
+    
 
 # vim: set sts=4 sw=4 ts=8 expandtab ft=python:
